a8899f0e98321a1630348bfeb7b113bafc408f36
Sergio Arcos <sergio.arcos@gmail.com>
2020-05-01 11:56:02 +0900

    rumi-comments

--- src/src/routes/_component/Logo.svelte
+++ src/src/routes/_component/Logo.svelte
@@ -7,7 +7,7 @@
         <img class="p-3 mx-auto d-block" src="./img/gato-logo-square.png" height="256" width="256"
              alt="logo">
         <div class="media-body align-self-center p-3">
-            <h1 class="display-1 font-weight-bold text-center">Green Cat Piano</h1>
+            <h1 class="display-3 font-weight-bold text-center">Green Cat Piano</h1>
             <p class="m-0">
                 {$_('logo.website')}
             </p>
