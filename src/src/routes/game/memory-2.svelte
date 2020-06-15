ea01850a52f99645ace4a70d4ca322f3fd419712
Sergio Arcos <sergio.arcos@gmail.com>
2020-06-16 00:37:06 +0900

    code

---
+++ src/src/routes/game/memory-2.svelte
@@ -0,0 +1,123 @@
+<script>
+    import {_} from 'svelte-i18n';
+
+    let imgDefaultUrl = 'game/memory-1-box.jpg';
+    let checkedUrl = 'game/memory-2-box.jpg';
+    let cards = [
+        {
+            id: 'cat',
+            imgUrl: 'game/memory-1-cat.jpg',
+            soundUrl: 'game/memory-1-cat.m4a'
+        },
+        {
+            id: 'lion',
+            imgUrl: 'game/memory-1-lion.jpg',
+            soundUrl: 'game/memory-1-lion.m4a'
+        },
+        {
+            id: 'mouse',
+            imgUrl: 'game/memory-1-mouse.jpg',
+            soundUrl: 'game/memory-1-mouse.m4a'
+        }
+    ];
+    cards = cards.concat(cards);
+    cards = cards
+            .map((a) => ({sort: Math.random(), value: a}))
+            .sort((a, b) => a.sort - b.sort)
+            .map((a) => a.value);
+
+    let remaining = 3;
+    let selected = null;
+    let disabled = false;
+
+    function startAgain() {
+        disabled = true;
+        setTimeout(async () => {
+            disabled = false;
+            console.log('startAgain');
+
+            [...Array(6).keys()].forEach((id) => {
+                document.getElementById(`card-${id}`).src = imgDefaultUrl;
+            });
+            remaining = 3;
+            selected = null;
+
+            cards = cards
+                    .map((a) => ({sort: Math.random(), value: a}))
+                    .sort((a, b) => a.sort - b.sort)
+                    .map((a) => a.value);
+        }, 2000);
+    }
+
+    function checkIfRight(id) {
+        disabled = true;
+        setTimeout(async () => {
+            disabled = false;
+            console.log('checkIfRight');
+
+            if (cards[id].id === cards[selected].id) {
+                // right
+                (new Audio('game/memory-1-right.m4a')).play();
+                remaining -= 1;
+                console.log('left:', remaining);
+                if (remaining <= 0) {
+                    startAgain();
+                }
+            } else {
+                // wrong
+                document.getElementById(`card-${id}`).src = imgDefaultUrl;
+                document.getElementById(`card-${selected}`).src = imgDefaultUrl;
+                (new Audio('game/memory-1-wrong.m4a')).play();
+            }
+            selected = null;
+        }, 1000);
+    }
+
+    function showCards(id) {
+        disabled = true;
+        setTimeout(async () => {
+            disabled = false;
+            console.log('showCards');
+
+            if (selected !== null) {
+                document.getElementById(`card-${id}`).src  = cards[id].imgUrl;
+                document.getElementById(`card-${selected}`).src  = cards[selected].imgUrl;
+                checkIfRight(id);
+            } else {
+                selected = id;
+            }
+        }, 1000);
+    }
+
+    function checkCard(id) {
+        console.log(id);
+        if (disabled) return;
+        const card = document.getElementById(`card-${id}`);
+        if (!card.src.includes(imgDefaultUrl)) return;
+
+        card.src = checkedUrl;
+        (new Audio(cards[id].soundUrl)).play();
+        showCards(id);
+    }
+</script>
+
+<div class="container">
+    <h1 class="text-center">{$_('nav.game.memory-2')}</h1>
+</div>
+
+<div class="container">
+    <div class="row row-cols-1 row-cols-md-3">
+        {#each cards as card, index}
+            <div class="col mb-4">
+                <div class="h-100 w-100">
+                    <img id="card-{index}" class="w-100" src="{imgDefaultUrl}" alt="{card.id}"
+                         on:click="{() => checkCard(index)}"/>
+                </div>
+            </div>
+        {/each}
+    </div>
+</div>
+
+<div class="container">
+    <p>Focus your attention on what sound is listening.</p>
+</div>
