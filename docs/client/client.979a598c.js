0e6a2931e2b297b6d89056b13101e9d32c920810
Sergio Arcos <sergio.arcos@gmail.com>
2020-07-29 23:26:14 +0900

    smaller logo

--- docs/client/client.979a598c.js
+++
@@ -1,1 +0,0 @@
-import"./client.0b66e212.js";
--- src/src/routes/_component/Logo.svelte
+++ src/src/routes/_component/Logo.svelte
@@ -3,17 +3,32 @@
 </script>
 
 <div class="container schoolbell">
-    <div class="row">
-        <img class="p-3 mx-auto d-block" src="./img/gato-logo-square.png" height="256" width="256"
+    <div class="d-flex mb-2">
+        <img class="p-1 h-100 w-25" src="./img/gato-logo-square.png"
              alt="logo">
-        <div class="media-body align-self-center p-3">
-            <h1 class="display-3 font-weight-bold text-center">Green Cat Piano</h1>
+        <div class="p-1">
+            <h1 class="font-weight-bold text-center">Green Cat Piano</h1>
             <p class="m-0">
                 {$_('logo.website')}
             </p>
             <p class="m-0">
                 {$_('logo.together')}
-            </p>
-        </div>
+            </p></div>
     </div>
 </div>
+
+<!--<div class="container schoolbell">-->
+<!--    <div class="fixed-relative">-->
+<!--        <img class="p-3 mx-auto d-sm" src="./img/gato-logo-square.png"-->
+<!--             alt="logo">-->
+<!--        <div class="p-3 media-body align-self-center d-sm">-->
+<!--            <h1 class="display-3 font-weight-bold text-center">Green Cat Piano</h1>-->
+<!--            <p class="m-0">-->
+<!--                {$_('logo.website')}-->
+<!--            </p>-->
+<!--            <p class="m-0">-->
+<!--                {$_('logo.together')}-->
+<!--            </p>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
